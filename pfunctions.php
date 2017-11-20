fad56df3bebf2ad71652e3ca5b21072bbb88dda3
Kevin Kodasma <kodakevi@greeny.cs.tlu.ee>
2017-11-20 09:24:34 +0200

    functions file, sisselogimise ja kasutajategemise functioniga

---
+++ pfunctions.php
@@ -0,0 +1,56 @@
+<?php
+	require("../../config.php");
+	$database = "if17_kodakevi";
+	
+	session_start();
+	
+	//SISSELOGIMISE FUNKTSIOON
+	function signIn($email, $password){
+		$notice = "";
+		//ühendus serveriga
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("SELECT id, email, password FROM projectusers WHERE email = ?");
+		$stmt->bind_param("s", $email);
+		$stmt->bind_result($id, $emailFromDb, $passwordFromDb);
+		$stmt->execute();
+		
+		//kontrollime vastavust
+		if ($stmt->fetch()){
+			$hash = hash("sha512", $password);
+			if ($hash == $passwordFromDb){
+				$notice = "Logisite sisse!";
+				
+				//määran sessiooni muutujad
+				$_SESSION["userId"] = $id;
+				$_SESSION["userEmail"] = $emailFromDb;
+				
+				//liigume edasi pealehele (main.php)
+				header("Location:main.php");
+				exit();
+			} else {
+				$notice = "Vale salasõna!";
+			}
+		} else {
+			$notice = 'Sellise kasutajatunnusega "' .$email .'"pole registreeritud!';
+		}
+		$stmt->close();
+		$mysqli->close();
+		return $notice;
+	}
+	
+	function signUp($signupFirstName, $signupFamilyName, $signupBirthDate, $gender, $signupEmail, $signupPassword){
+		//loome andmebaasiühenduse
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		//valmistame ette käsu andmebaasiserverile
+		$stmt = $mysqli->prepare("INSERT INTO projectusers (firstname, lastname, birthday, gender, email, password) VALUES (?, ?, ?, ?, ?, ?)");
+		echo $mysqli->error;
+		$stmt->bind_param("sssiss", $signupFirstName, $signupFamilyName, $signupBirthDate, $gender, $signupEmail, $signupPassword);
+		if($stmt->execute()){
+			echo "\n Õnnestus!";
+		} else {
+			echo "\n Tekkis viga:" .$stmt->error;
+		}
+		$stmt->close();
+		$mysqli->close();
+	}
+?>
