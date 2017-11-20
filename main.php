bc3f1ab8ada0f5fd5e8e2f17e03e75aec53a4cf7
Krislyn <lukskris@greeny.cs.tlu.ee>
2017-11-20 10:04:15 +0200

    nupud

--- main.php
+++ main.php
@@ -1,3 +1,31 @@
 <?php
 
+
+
 ?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Koduloomade blogi
+	</title>
+</head>
+<body>
+
+<h1>Koduloomade blogi Sinu lemmikule.</h1>
+<p>Veebileht on loodud oma koduloomade näitamiseks teistele.</p>
+
+<h2>Kui kasutaja on juba olemas, siis</h2>
+<p><a href="login.php">logi sisse</a>!</p>
+
+<h2>Kui kasutajat pole veel registreeritud</h2>
+<p><a href="signup.php">loo kasutaja</a>!</p>
+
+
+</body>
+</html>
