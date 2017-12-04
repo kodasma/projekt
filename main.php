d3792fb4238efd624ab639b27e88a79b92c2dad3
Kertu Kipper <phkel@users.noreply.github.com>
2017-12-04 17:10:29 +0200

    õige rel

--- main.php
+++ main.php
@@ -14,7 +14,7 @@
 	<title>
 		Koduloomade blogi
 	</title>
-	<link rel="stylesheet" href="kmain.css">
+	<link rel="stylesheet" href="css/main.css">
 	<link href="https://fonts.googleapis.com/css?family=Telex&amp;subset=latin-ext" rel="stylesheet">
 </head>
 <body>
